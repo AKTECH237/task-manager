afb917a48de41658d84f8a80f35eb21d351ebcb5
Aurelien Kevin Tagne <https://github.com/AKTECH237>
2026-09-18 10:49:18 +0200

    chore: initialize Spring Boot backend

---
+++ src/main/java/com/akt/task_manager_api/repository/TaskRepository.java
@@ -0,0 +1,21 @@
+package com.akt.task_manager_api.repository;
+
+import com.akt.task_manager_api.entity.Task;
+import com.akt.task_manager_api.entity.TaskStatus;
+import com.akt.task_manager_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUserOrderByCreatedAtDesc(User user);
+
+    List<Task> findByUserAndStatusOrderByCreatedAtDesc(
+            User user,
+            TaskStatus status
+    );
+
+    Optional<Task> findByIdAndUser(Long id, User user);
+}
