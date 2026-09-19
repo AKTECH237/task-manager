024ce9ce088cf51d499cc121491a045ec6b54c71
Aurelien Kevin Tagne <https://github.com/AKTECH237>
2026-09-19 02:24:54 +0200

    feat: fix task status enum, security config and complete core features

--- src/main/java/com/akt/task_manager_api/security/JwtService.java
+++ src/main/java/com/akt/task_manager_api/security/JwtService.java
@@ -15,10 +15,10 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    @Value("${jwt.secret}")
+    @Value("${JWT_SECRET:404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970}")
     private String secretKey;
 
-    @Value("${jwt.expiration}")
+    @Value("${jwt.expiration:86400000}")
     private long jwtExpiration;
 
     public String extractUsername(String token) {
